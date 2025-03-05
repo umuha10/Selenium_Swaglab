143eec567e7b5f5efc6ba6cd82c37bb3801deb75
unknown <umuhabibah1010@gmail.com>
2025-03-05 19:03:12 +0700

    verify shopping detail part 1

--- Script.py
+++ Script.py
@@ -21,36 +21,57 @@ driver.maximize_window()
 # ---------
 
 # Login (Negative Case #1)
-username_field = driver.find_element(By.ID, "user-name").send_keys("umu")
-password_field = driver.find_element(By.ID, "password").send_keys("habibah")
-login_button = driver.find_element(By.ID, "login-button").click()
+username_field = driver.find_element(By.ID, "user-name")
+username_field.send_keys("umu")
+
+password_field = driver.find_element(By.ID, "password")
+password_field.send_keys("habibah")
+
+login_button = driver.find_element(By.ID, "login-button")
+login_button.click()
 time.sleep(2)
 
 # Login (Negative Case #2)
 driver.refresh()
-username_field = driver.find_element(By.ID, "user-name").send_keys("umu")
-password_field = driver.find_element(By.ID, "password").send_keys("secret_sauce")
-login_button = driver.find_element(By.ID, "login-button").click()
+username_field = driver.find_element(By.ID, "user-name")
+username_field.send_keys("umu")
+
+password_field = driver.find_element(By.ID, "password")
+password_field.send_keys("secret_sauce")
+
+login_button = driver.find_element(By.ID, "login-button")
+login_button.click()
 time.sleep(2)
 
 # Login (Negative Case #3)
 driver.refresh()
-username_field = driver.find_element(By.ID, "user-name").send_keys("standard_user")
-password_field= driver.find_element(By.ID, "password").send_keys("123")
+username_field = driver.find_element(By.ID, "user-name")
+username_field.send_keys("standard_user")
+
+password_field= driver.find_element(By.ID, "password")
+password_field.send_keys("123")
+
 login_button = driver.find_element(By.ID, "login-button").click()
 time.sleep(2)
 
 # Login (Negative Case #4)
 driver.refresh()
-username_field = driver.find_element(By.ID, "user-name").send_keys("")
-password_field = driver.find_element(By.ID, "password").send_keys("")
+username_field = driver.find_element(By.ID, "user-name")
+username_field.send_keys("")
+
+password_field = driver.find_element(By.ID, "password")
+password_field.send_keys("")
+
 login_button = driver.find_element(By.ID, "login-button").click()
 time.sleep(2)
 
 # Login (Possitive Case #5)
 driver.refresh
-username_field = driver.find_element(By.ID, "user-name").send_keys("standard_user")
-password_field = driver.find_element(By.ID, "password").send_keys("secret_sauce")
+username_field = driver.find_element(By.ID, "user-name")
+username_field.send_keys("standard_user")
+
+password_field = driver.find_element(By.ID, "password")
+password_field.send_keys("secret_sauce")
 login_button = driver.find_element(By.ID, "login-button").click()
 
 # ------------------------
@@ -58,89 +79,148 @@ login_button = driver.find_element(By.ID, "login-button").click()
 # ------------------------
 
 # Button link All Item (Positive case #6)
-burger_button = driver.find_element(By.CLASS_NAME, "bm-burger-button").click()
+burger_button = driver.find_element(By.CLASS_NAME, "bm-burger-button")
+burger_button.click()
 
 # Button link All item (Possitive case #7)
 wait = WebDriverWait(driver, 10)
-all_item = wait.until(EC.element_to_be_clickable((By.ID, "inventory_sidebar_link"))).click()
+all_item = wait.until(EC.element_to_be_clickable((By.ID, "inventory_sidebar_link")))
+all_item.click()
 
 # Button link All item (Possitive case #8)
-burger_button = driver.find_element(By.CLASS_NAME, "bm-burger-button").click()
+burger_button = driver.find_element(By.CLASS_NAME, "bm-burger-button")
+burger_button.click()
+
 wait = WebDriverWait(driver, 10)
-about_button = wait.until(EC.element_to_be_clickable((By.ID, "about_sidebar_link"))).click()
+about_button = wait.until(EC.element_to_be_clickable((By.ID, "about_sidebar_link")))
+about_button.click()
+
 driver.back()
 time.sleep(2)
 
 # Button icon close (Possitive case #9)
-close_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "bm-cross-button"))).click()
+close_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "bm-cross-button")))
+close_button.click()
 
 # Button logout (Possitive case #10)
-burger_button = driver.find_element(By.CLASS_NAME, "bm-burger-button").click()
-logout_button = wait.until(EC.element_to_be_clickable((By.ID, "logout_sidebar_link"))).click()
+burger_button = driver.find_element(By.CLASS_NAME, "bm-burger-button")
+burger_button.click()
+
+logout_button = wait.until(EC.element_to_be_clickable((By.ID, "logout_sidebar_link")))
+logout_button.click()
 
 # -----------------
 # | VERIFY FILTER |
 # -----------------
 
 # Dropdown filter (Possitive case #11)
-username_field = driver.find_element(By.ID, "user-name").send_keys("standard_user")
-password_field = driver.find_element(By.ID, "password").send_keys("secret_sauce")
-login_button = driver.find_element(By.ID, "login-button").click()
-time.sleep(5)
+username_field = driver.find_element(By.ID, "user-name")
+username_field.send_keys("standard_user")
+
+password_field = driver.find_element(By.ID, "password")
+password_field.send_keys("secret_sauce")
+
+login_button = driver.find_element(By.ID, "login-button")
+login_button.click()
+
+time.sleep(2)
+
 dropdown = Select(driver.find_element(By.CLASS_NAME, "product_sort_container"))
 dropdown.select_by_value("hilo")
-time.sleep(5)
+time.sleep(2)
+
 dropdown = Select(driver.find_element(By.CLASS_NAME, "product_sort_container"))
 dropdown.select_by_value("lohi")
-time.sleep(5)
+time.sleep(2)
+
 dropdown = Select(driver.find_element(By.CLASS_NAME, "product_sort_container"))
 dropdown.select_by_value("za")
-time.sleep(5)
+time.sleep(2)
+
 dropdown = Select(driver.find_element(By.CLASS_NAME, "product_sort_container"))
 dropdown.select_by_value("az")
-time.sleep(5)
+time.sleep(2)
 
 # ------------------
 # | PRODUCT DETAIL |
 # ------------------
 # Product detail (Possitive case #12)
-product_detail = driver.find_element(By.CLASS_NAME, "inventory_item_name").click()
-time.sleep(5)
-product_detail_add = driver.find_element(By.CSS_SELECTOR, "#inventory_item_container > div > div > div > button").click()
-time.sleep(7)
-product_detail_remove = driver.find_element(By.CSS_SELECTOR, "#inventory_item_container > div > div > div > button").click()
-time.sleep(7)
-product_detail_back = driver.find_element(By.CLASS_NAME, "inventory_details_back_button").click()
-time.sleep(5)
+product_detail = driver.find_element(By.CLASS_NAME, "inventory_item_name")
+product_detail.click()
+time.sleep(2)
+
+product_detail_add = driver.find_element(By.CSS_SELECTOR, "#inventory_item_container > div > div > div > button")
+product_detail_add.click()
+time.sleep(2)
+
+product_detail_remove = driver.find_element(By.CSS_SELECTOR, "#inventory_item_container > div > div > div > button")
+product_detail_remove.click()
+time.sleep(2)
+
+product_detail_back = driver.find_element(By.CLASS_NAME, "inventory_details_back_button")
+product_detail_back.click()
+time.sleep(2)
 
 # --------------------------
 # | PRODUCT ADD AND REMOVE |
 # --------------------------
 product_add = driver.find_element(By.CSS_SELECTOR, "#inventory_container > div > div:nth-child(1) > div.pricebar > button").click()
-time.sleep(3)
+time.sleep(2)
 product_add = driver.find_element(By.CSS_SELECTOR, "#inventory_container > div > div:nth-child(2) > div.pricebar > button").click()
-time.sleep(3)
+time.sleep(2)
 product_add = driver.find_element(By.CSS_SELECTOR, "#inventory_container > div > div:nth-child(3) > div.pricebar > button").click()
-time.sleep(3)
+time.sleep(2)
 product_add = driver.find_element(By.CSS_SELECTOR, "#inventory_container > div > div:nth-child(4) > div.pricebar > button").click()
-time.sleep(3)
+time.sleep(2)
 product_add = driver.find_element(By.CSS_SELECTOR, "#inventory_container > div > div:nth-child(5) > div.pricebar > button").click()
-time.sleep(3)
+time.sleep(2)
 product_add = driver.find_element(By.CSS_SELECTOR, "#inventory_container > div > div:nth-child(6) > div.pricebar > button").click()
-time.sleep(3)
+time.sleep(2)
 product_remove = driver.find_element(By.CSS_SELECTOR, "#inventory_container > div > div:nth-child(1) > div.pricebar > button").click()
-time.sleep(3)
+time.sleep(2)
 product_remove = driver.find_element(By.CSS_SELECTOR, "#inventory_container > div > div:nth-child(2) > div.pricebar > button").click()
-time.sleep(3)
+time.sleep(2)
 product_remove = driver.find_element(By.CSS_SELECTOR, "#inventory_container > div > div:nth-child(3) > div.pricebar > button").click()
-time.sleep(3)
+time.sleep(2)
 product_remove = driver.find_element(By.CSS_SELECTOR, "#inventory_container > div > div:nth-child(4) > div.pricebar > button").click()
-time.sleep(3)
+time.sleep(2)
 product_remove = driver.find_element(By.CSS_SELECTOR, "#inventory_container > div > div:nth-child(5) > div.pricebar > button").click()
 time.sleep(3)
 product_remove = driver.find_element(By.CSS_SELECTOR, "#inventory_container > div > div:nth-child(6) > div.pricebar > button").click()
-time.sleep(3)
+time.sleep(2)
 
+# ------------------------
+# | VERIFY SHOPPING DETAIL |
+# ------------------------
+
+shopping_cart = driver.find_element(By.CSS_SELECTOR,"#shopping_cart_container > a > svg").click()
+continue_shopping = driver.find_element(By.CSS_SELECTOR, "#cart_contents_container > div > div.cart_footer > a.btn_secondary").click()
+product_add = driver.find_element(By.CSS_SELECTOR, "#inventory_container > div > div:nth-child(1) > div.pricebar > button").click()
+time.sleep(2)
+product_add = driver.find_element(By.CSS_SELECTOR, "#inventory_container > div > div:nth-child(4) > div.pricebar > button").click()
+time.sleep(2)
+product_add = driver.find_element(By.CSS_SELECTOR, "#inventory_container > div > div:nth-child(2) > div.pricebar > button").click()
+time.sleep(2)
+shopping_cart = driver.find_element(By.CSS_SELECTOR,"#shopping_cart_container > a > svg").click()
+shopping_detail_remove = driver.find_element(By.CSS_SELECTOR, "#cart_contents_container > div > div.cart_list > div:nth-child(4) > div.cart_item_label > div.item_pricebar > button").click()
+checkout = driver.find_element(By.CSS_SELECTOR, "#cart_contents_container > div > div.cart_footer > a.btn_action.checkout_button")
+checkout.click()
+cancel_checkout = driver.find_element(By.CSS_SELECTOR, "#checkout_info_container > div > form > div.checkout_buttons > a").click()
+checkout = driver.find_element(By.CSS_SELECTOR, "#cart_contents_container > div > div.cart_footer > a.btn_action.checkout_button")
+checkout.click()
+
+# Send Information 
+first_name = driver.find_element(By.ID, "first-name")
+first_name.send_keys("Salsa")
+
+last_name = driver.find_element(By.ID, "last-name")
+last_name.send_keys("Khairina")
+
+postal_code = driver.find_element(By.ID, "postal-code")
+postal_code.send_keys("56768")
+
+continue_checkout = driver.find_element(By.CSS_SELECTOR, "#checkout_info_container > div > form > div.checkout_buttons > input")
+continue_checkout.click()
 
 input("tekan enter untuk keluar...")
 
